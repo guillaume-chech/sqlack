5090f30dc6c36f0f361f898d527d8cd2d7fd7d71
gchech <guillaume.chevalier2@gmail.com>
2020-02-11 16:17:03 +1300

    Added psycopg2 and Redshift utils

---
+++ credentials.py
@@ -0,0 +1,10 @@
+###############################################
+# Credentials for Redshift. We Must use SECRET service in the future 
+###############################################
+
+DB_CONNECTION = {
+    'db_host': 'myhost.redshift.amazonaws.com', 
+    'db_name': 'somedb', 
+    'db_username': 'user',
+    'db_password': 'pA$$word'
+}
