fc64e1b7d4a595745d1a779f41f26490c62a9391
gchech <guillaume.chevalier2@gmail.com>
2020-02-13 17:51:52 +1300

    commented tests

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from pg_client.pg_client import PGClient
-from pg_client.credentials import DB_CREDENTIALS
-from tabulate import tabulate
+# from pg_client.pg_client import PGClient
+# from pg_client.credentials import DB_CREDENTIALS
+# from tabulate import tabulate
 
-query = "SELECT id,created_at from backend.users limit 10"
-client = PGClient(credentials=DB_CREDENTIALS)
-result = client.execute_query(query)
-print(tabulate(tabular_data=result["data"],headers=result["headers"],tablefmt="psql"))
+# query = "SELECT id,created_at from backend.users limit 10"
+# client = PGClient(credentials=DB_CREDENTIALS)
+# result = client.execute_query(query)
+# print(tabulate(tabular_data=result["data"],headers=result["headers"],tablefmt="psql"))
