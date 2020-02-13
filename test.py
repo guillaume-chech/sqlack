b9597cac835b6b113a5dbfd8069fac7b7321cef4
gchech <guillaume.chevalier2@gmail.com>
2020-02-13 17:32:18 +1300

    Added missing files

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from pg_client.pg_client import PGClient
 from pg_client.credentials import DB_CREDENTIALS
+from tabulate import tabulate
 
-query = "SELECT 1"
+query = "SELECT id,created_at from backend.users limit 10"
 client = PGClient(credentials=DB_CREDENTIALS)
-con = client.create_connection()
-print(client.execute_query(con,query))
+result = client.execute_query(query)
+print(tabulate(tabular_data=result["data"],headers=result["headers"],tablefmt="psql"))
